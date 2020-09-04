276de96d193b067ab39e4e93cb2eebd6a34c0d91
Wsasa <1327685774@qq.com>
2020-09-04 10:46:54 +0800

    userover

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -19,7 +19,7 @@ $.ajaxPrefilter(function (options) {
   // 当请求结束后，判断用户的设置访问权限
   options.complete = function (res) {
     // 在complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
-    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败!') {
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
       // 强制清空token
       localStorage.removeItem('token')
       // 强制跳转到登录页面
---
+++ assets/js/user/user_info.js
@@ -0,0 +1,53 @@
+$(function () {
+    const form = layui.form
+    const layer = layui.layer
+    form.verify({
+        nickname(value) {
+            if (value.length > 16) {
+                return '昵称长度必须在1~6个字符之间'
+            }
+        }
+    })
+    initUserInfo()
+    // 初始化用户信息
+    function initUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败!')
+                }
+                console.log(res);
+                form.val('formUserInfo', res.data)
+            }
+
+        })
+    }
+    // 重置表单数据
+    $('#btnReset').on('click', function (e) {
+        e.preventDefault()
+        initUserInfo()
+    })
+
+    // 监听表单的提交事件
+    $('.layui-form').on('submit', function (e) {
+        // 阻止表单的默认提交行为
+        e.preventDefault()
+        // 发起ajax数据请求
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新用户信息失败！')
+                }
+                layer.msg('更新用户信息成功！')
+
+                // 调用父页面中的方法，重新渲染永固的头像和用户的学习
+                window.parent.getUserInfo()
+            }
+        })
+    })
+})
